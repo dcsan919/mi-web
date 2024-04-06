cac9555c6ec21fc80e92d18fbc065affea0f5973
David Cen Pool <cendavid919@gmail.com>
2024-04-06 15:29:01 -0600

    agregando método get

--- index.js
+++ index.js
@@ -13,5 +13,6 @@ port = 9000;
 app.listen(port, ()=> console.log(`Servidor en el puerto: ${port}`))
 
 app.get('/', (req, res)=>{
-  res.send('hola david santiago')
+  res.send('hola david santiago');
+  res.send("de donde eres");
 })
